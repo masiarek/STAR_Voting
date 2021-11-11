a5e775aff69c678c9354738f5bc0c0a7d6160f01
Arend Peter Castelein <apc1993@gmail.com>
2021-11-10 20:23:02 -0800

    Add SIMPLE_STAR support and refactored script

--- STAR_Voting_Arend_Adam_.py
+++ STAR_Voting_Arend_Adam_.py
@@ -1,4 +1,6 @@
 import csv
+import re
+from io import StringIO
 from copy import deepcopy
 import json
 import sys
@@ -10,121 +12,201 @@ def format_percent(count, total):
     return f'{round(100 * count / total)}%'
 
 
-filename = sys.argv[1]
-''',A,B,C
-2,0,2,5
-3,5,4,3
-'''
+def load_ballots_from_file(filename):
+    """
+    SIMPLE STAR format examples
+    n,A,B,C
+    2,0,2,5
+    3,5,4,3
 
-'''expected results - report:
-# of voters who prefer  A B C
-A over                  - 3 3
-B over                  2 - 3
-C over                  2 2 -
-'''
+    n:A,B,C
+    2:0,2,5
+    3:5,4,3
 
-# Load CSV
-ballots = []
-with open(filename, 'r') as f:
-    rows = csv.reader(f)
+    A   B   C
+    0   2   5
+    5   4   3
+    """
+    
+    # Parse SIMPLE STAR as CSV
+    valid_delims = ['\s', ':', ',']
+    # https://stackoverflow.com/questions/2785755/how-to-split-but-ignore-separators-in-quoted-strings-in-python
+    # https://regexr.com/69a27
+    PATTERN = re.compile(f'''[{"".join(valid_delims)}][\s]*(?=(?:[^'"]|'[^']*'|"[^"]*")*$)''')
+    def clean_line(line):
+        line = line.replace('\n', '')
+        return ','.join(re.split(PATTERN, line))
+
+    with open(filename, 'r') as f:
+        lines = list(clean_line(l) for l in f.readlines() if l != '\n' and l[0] != '#')
+        csv_f = StringIO('\n'.join(lines))
+
+    # Load CSV
+    ballots = []
+    rows = csv.reader(csv_f)
+    is_weighted = None
     for i, row in enumerate(rows):
         if i == 0:
-            candidates = row[1:]
+            is_weighted = row[0] == 'Number_of_Ballots'
+            candidates = row[1:] if is_weighted else row
         else:
-            ballot_count = int(row[0])
+            ballot_count = int(row[0]) if is_weighted else 1
+            scores = row[1:] if is_weighted else row
+            scores = list(int(s) for s in scores)
             for _ in range(ballot_count):
-                ballots.append(row[1:])
+                ballots.append(scores)
 
-# Plurality
-counts = [0] * len(candidates)
-for ballot in ballots:
-    top_candidate = ballot.index(max(ballot))
-    counts[top_candidate] = counts[top_candidate] + 1
-winning_candidate_index = counts.index(max(counts))
-print(f"# PLURALITY: {candidates[winning_candidate_index]}")
-total_votes = sum(counts)
-print(f"\t## Counts")
-for candidate, count in zip(candidates, counts):
-    print(f'\t{candidate}: {count} ({format_percent(count, total_votes)})')
-print()
+    return candidates, ballots
 
-# Instant Runoff
-counts = [0] * len(candidates)
-round_counts = []
-counts = [0] * len(candidates)
-for ballot in ballots:
-    top_candidate = ballot.index(max(ballot))
-    counts[top_candidate] = counts[top_candidate] + 1
-round_counts.append(counts)
-ballot_copies = deepcopy(ballots)
-while max(counts) < (total_votes / 2):
-    # clear min candidate
-    bot_candidate = counts.index(min(counts))
-    for ballot in ballot_copies:
-        ballot[bot_candidate] = '0'  # I'm choosing string because they're read as string from the csv
-    counts = [0] * len(candidates)
-    for ballot in ballot_copies:
-        top_candidate = ballot.index(max(ballot))
-        counts[top_candidate] = counts[top_candidate] + 1
-    round_counts.append(counts)
-winning_candidate_index = counts.index(max(counts))
-print(f"# IRV: {candidates[winning_candidate_index]}")
-total_votes = sum(counts)
-for i, counts in enumerate(round_counts):
-    print(f"\t## Round {i + 1} Counts")
-    for candidate, count in zip(candidates, counts):
-        print(f'\t{candidate}: {count} ({format_percent(count, total_votes)})')
+
+def get_runoff_counts(ballots, valid_candidates=None):
+    counts = [0] * len(ballots[0])
+    if valid_candidates is None:
+        valid_candidates = set(range(len(candidates)))
+
+    for ballot in ballots:
+        top_candidates = []
+        top_candidate_score = -1
+        for candidate, score in enumerate(ballot):
+            if candidate not in valid_candidates:
+                continue
+
+            if top_candidate_score < score:
+                top_candidates = [candidate]
+                top_candidate_score = score
+            elif len(top_candidates) > 0 and top_candidate_score == score:
+                top_candidates.append(candidate)
+
+        # Condition 1: len(top_candidates) == 1
+        ### FPTP: More than 1 top candidate is impossible
+        ### RCV:  More than 1 top candidate is impossible
+        ### STAR: Vote is discarded as "no preference" when there are multiple top candidates
+
+        # Condition 2: top_candidate_score > 0
+        ### FPTP: Valid ballots should have someone selected, so this scenario is impossible
+        ### RCV: This can happen in a exhausted ballot scenario, in which case it should be discarded
+        ### STAR: It's impossible to meet this criteria without failing condition 1
+
+        if len(top_candidates) == 1 and top_candidate_score > 0:
+            counts[top_candidates[0]] = counts[top_candidates[0]] + 1
+
+    rankings = sorted(list(valid_candidates), key=lambda candidate: counts[candidate], reverse=True)
+    return counts, rankings
+
+
+def get_sum_counts(ballots):
+    counts = [0] * len(ballots[0])
+    for ballot in ballots:
+        for i, stars in enumerate(ballot):
+            counts[i] = counts[i] + int(stars)
+
+    rankings = sorted(list(range(len(counts))), key=lambda candidate: counts[candidate], reverse=True)
+    return counts, rankings
+
+
+def print_plurality_results(candidates, ballots):
+    counts, rankings = get_runoff_counts(ballots)
+    print(f"# PLURALITY: {candidates[rankings[0]]}")
+    print(f"\t## Counts")
+    for c_index in rankings:
+        print(f'\t{candidates[c_index]}: {counts[c_index]} ({format_percent(counts[c_index], len(ballots))})')
     print()
 
-# Star
-## Sum all ballots
-counts = [0] * len(candidates)
-for ballot in ballots:
-    for i, stars in enumerate(ballot):
-        counts[i] = counts[i] + int(stars)
-## Get 2 highest (this approach is messy, I'm sure there's a better one)
-highest = counts.index(max(counts))
-t = counts[highest]
-counts[highest] = 0  # removing highest, so that I can find next highest
-next_highest = counts.index(max(counts))
-counts[highest] = t  # adding highest count back
-## Automatic Runoff
-highest_count = 0
-next_highest_count = 0
-for ballot in ballots:
-    if ballot[highest] > ballot[next_highest]:
-        highest_count = highest_count + 1
-    if ballot[highest] < ballot[next_highest]:
-        next_highest_count = next_highest_count + 1
-no_pref_count = total_votes - highest_count - next_highest_count
-if highest_count > next_highest_count:
-    winning_candidate_index = highest
-else:
-    winning_candidate_index = next_highest
-print(f"# STAR: {candidates[winning_candidate_index]}")
-print(f"\t## Runoff")
-print(f"\t{candidates[highest]}: {highest_count} ({format_percent(highest_count, total_votes)})")
-print(f"\t{candidates[next_highest]}: {next_highest_count} ({format_percent(next_highest_count, total_votes)})")
-print(f"\tNo Preference: {no_pref_count} ({format_percent(no_pref_count, total_votes)})")
-print()
-print(f"\t## Sums")
-for candidate, count in zip(candidates, counts):
-    print(f"\t{candidate}: {count} (avg: {round(100 * count / total_votes) / 100})")
 
-print()
+def print_irv_results(candidates, ballots):
+    counts, rankings = get_runoff_counts(ballots)
+    rounds = [(counts, rankings)]
+    valid_candidates = set(list(range(len(candidates))))
 
-# Concordant Matrix
+    while max(counts) < (len(ballots) / 2):
+        valid_candidates.remove(rankings[-1]) 
+        counts, rankings = get_runoff_counts(ballots, valid_candidates=valid_candidates)
+        rounds.append((counts, rankings))
 
-## c1 prefered over c2
-pref_count = [['-' if c1 == c2 else 0 for c2 in range(len(candidates))] for c1 in range(len(candidates))]
+    winning_candidate_index = counts.index(max(counts))
+    print(f"# IRV: {candidates[winning_candidate_index]}")
+    for i, round_pair in enumerate(rounds):
+        counts = round_pair[0]
+        rankings = round_pair[1]
+        print(f"\t## Round {i + 1} Counts")
+        for c_index in rankings:
+            print(f'\t{candidates[c_index]}: {counts[c_index]} ({format_percent(counts[c_index], len(ballots))})')
+        print()
 
-for ballot in ballots:
-    for (c1, n1), (c2, n2) in itertools.product(list(enumerate(ballot)), list(enumerate(ballot))):
-        if n1 > n2:
-            pref_count[c1][c2] = pref_count[c1][c2] + 1
 
-col_width = max([len(c) for c in candidates] + [len("Prefers")]) + 3
-print('# CONCORDANT MATRIX')
-print('\tPrefers'.ljust(col_width+len(' over')) + ''.join(c.ljust(col_width) for c in candidates))
-for i, c in enumerate(candidates):
-    print(f'\t{c} over'.ljust(col_width+len(' over')) + ''.join(str(j).ljust(col_width) for j in pref_count[i]))
+def print_star_results(candidates, ballots):
+    ## Sum all ballots
+    sum_counts, sum_rankings = get_sum_counts(ballots)
+
+    ## Automatic Runoff
+    runoff_counts, runoff_rankings = get_runoff_counts(ballots, valid_candidates=sum_rankings[:2])
+
+    ## Display Results
+    print(f"# STAR: {candidates[runoff_rankings[0]]}")
+    
+    print(f"\t## Sums")
+    for c_index in sum_rankings:
+        print(f"\t{candidates[c_index]}: {sum_counts[c_index]} (avg: {round(100 * sum_counts[c_index] / len(ballots)) / 100})")
+    print()
+
+    print(f"\t## Runoff")
+    for c_index in sum_rankings[:2]:
+        print(f"\t{candidates[c_index]}: {runoff_counts[c_index]} ({format_percent(runoff_counts[c_index], len(ballots))})")
+    no_pref_count = len(ballots) - runoff_counts[runoff_rankings[0]] - runoff_counts[runoff_rankings[1]]
+    print(f"\tNo Preference: {no_pref_count} ({format_percent(no_pref_count, len(ballots))})")
+
+    print()
+
+
+def print_concordant_matrix(candidates, ballots):
+    '''
+    Example
+        expected results - report:
+        # of voters who prefer  A B C
+        A over                  - 3 3
+        B over                  2 - 3
+        C over                  2 2 -
+    '''
+
+    def try_bold(s, count1, count2): # the bold effect didn't look super good, so I'm disabling it
+        return s
+        """
+        if count1 > count2:
+            return f'\033[1m{s}\033[0m'
+        else:
+            return s
+        """
+
+    ## c1 prefered over c2
+    pref_count = [['-' if c1 == c2 else 0 for c2 in range(len(candidates))] for c1 in range(len(candidates))]
+
+    for c1 in range(len(candidates)):
+        for c2 in range(c1+1, len(candidates)):
+            counts, _ = get_runoff_counts(ballots, valid_candidates=[c1, c2])
+
+            pref_count[c1][c2] = counts[c1] 
+            pref_count[c2][c1] = counts[c2]
+
+
+    col_width = max([len(c) for c in candidates] + [len("Prefers")]) + 3
+    print('# CONCORDANT MATRIX')
+    print('\tPrefers'.ljust(col_width+len(' over')) + ''.join(c.ljust(col_width) for c in candidates))
+    for row_can, row_name in enumerate(candidates):
+        prefix = f'\t{row_name} over'.ljust(col_width+len(' over'))
+        print(prefix + ''.join(
+                try_bold(str(count).ljust(col_width), count, pref_count[col_can][row_can])
+                for col_can, count in enumerate(pref_count[row_can])
+            )
+        )
+
+
+if __name__ == '__main__':
+    candidates, ballots = load_ballots_from_file(sys.argv[1]) 
+
+    print_plurality_results(candidates, ballots)
+
+    print_irv_results(candidates, ballots)
+
+    print_star_results(candidates, ballots)
+
+    print_concordant_matrix(candidates, ballots)
